828357aa8a8842776cfdd369e70f35b17e8ca4d1
yash kumar <you@example.com>
2026-01-30 21:28:21 +0530

    user file

---
+++ 01.user.ts
@@ -0,0 +1,9 @@
+var age= 21;   //declaretion with type inference in TypeScript and javaScript
+var age: number= 21; 
+var FullName: string= "Yash Kumar"; 
+var CollegeName: string= "SRC";
+
+console.log(`Age is: ${age}`);
+console.log(`Age is: ${age}`);
+console.log(`Name is: ${FullName}`);
+console.log(`College Name is: ${CollegeName}`);
