ecce7be4358a767d1d410e60d95e313cb0a23c1a
yash kumar <you@example.com>
2026-01-30 21:33:52 +0530

    tuple data type

---
+++ 05.TuppleDataType.ts
@@ -0,0 +1,13 @@
+var tup1:[number,string,boolean] = [101,"Hello",true];
+var empData:[number,string,boolean][] = [
+    [201,"Yash",true],
+    [202,"Yash Kumar",false],
+    [203,"Kumar",true]
+];
+console.log("Tupple 1:",tup1);
+console.log("Employee Data:",empData);
+
+// Accessing Tupple Elements
+console.log("Tupple First Element:",tup1[0]);
+console.log("Tupple Second Element:",tup1[1]);
+console.log("Tupple Third Element:",tup1[2]);
