1dac7493eaee343eb683610f3172a1b93053ae07
yash kumar <you@example.com>
2026-01-30 21:34:48 +0530

    object data type

---
+++ 06.ObjectDataType.ts
@@ -0,0 +1,14 @@
+var userData:{name:string, age:number , address:{street:string, city:string, country:string}} = {
+    name: "Yash",
+    age: 21,
+    address:{
+        street: "Vaishai Road",
+        city: "Vaishali",
+        country: "India"
+    }
+};
+
+userData.age = 22;
+
+console.log("User Data:", userData);
+
