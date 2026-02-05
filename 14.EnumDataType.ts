81a3e7c1f176bb6bf1b143276bdabf7b4768febe
yash kumar <you@example.com>
2026-02-05 21:37:09 +0530

    enum data type

---
+++ 14.EnumDataType.ts
@@ -0,0 +1,10 @@
+enum whoType{
+    student="student",
+    teacher="teacher",
+    management="management",
+    labStaff="labStaff",
+};
+
+var who:whoType=whoType.teacher;
+who=whoType.teacher;
+console.log(whoType.teacher);
